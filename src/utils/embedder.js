dfd32d5bff64214b3fee52de336280d79edca183
Azeem Shaik <azeemshaik025@gmail.com>
2025-01-25 15:02:33 +0530

    chore: add google generative text embedding model

---
+++ src/utils/embedder.js
@@ -0,0 +1,5 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API);
+const model = genAI.getGenerativeModel({ model: "text-embedding-004" });
+export default model;
